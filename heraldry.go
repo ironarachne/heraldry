10f50c3affec13c30753c3011c2cad4c2bc5c85b
Ben Overmyer <ben@benovermyer.com>
2018-03-19 18:15:23 -0500

    Improve the appearance of Or and purpure

--- heraldry.go
+++ heraldry.go
@@ -38,13 +38,13 @@ type Device struct {
 }
 
 var (
-	Or                 = Tincture{"metal", "Or", "#FFFF00"}
+	Or                 = Tincture{"metal", "Or", "#FFEC00"}
 	Argent             = Tincture{"metal", "argent", "#FFFFFF"}
 	Sable              = Tincture{"color", "sable", "#000000"}
 	Gules              = Tincture{"color", "gules", "#EE0000"}
 	Vert               = Tincture{"color", "vert", "#009900"}
 	Azure              = Tincture{"color", "azure", "#0000FF"}
-	Purpure            = Tincture{"color", "purpure", "#CC00CC"}
+	Purpure            = Tincture{"color", "purpure", "#831CA6"}
 	Metals             = [2]Tincture{Or, Argent}
 	Colors             = [5]Tincture{Sable, Gules, Vert, Azure, Purpure}
 	AvailableTinctures = [7]Tincture{Or, Argent, Sable, Gules, Vert, Azure, Purpure}
